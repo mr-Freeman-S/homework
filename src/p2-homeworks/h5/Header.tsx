4fcdac10363fb768eac072a3a30e838fe2d65dde
mr-Freeman-S <mr.baranok@mail.ru>
2022-03-04 23:47:46 +0300

    fix routes in 5HM and start make hamburger menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from "./Routes";
+import s from './Header.module.css'
+
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    return (<div style={{display: "inline-block"}}>
+            <div className={s.mainDiv}>
+                <NavLink to={PATH.PRE_JUNIOR}>PreJunior</NavLink>
+                <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+            </div>
+            <div className={s.buttonSlide}></div>
         </div>
+
     )
 }
 
