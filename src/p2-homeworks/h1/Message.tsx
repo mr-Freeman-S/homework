96f5a2f7fff81d805e99f4695513f8886a25ca83
mr-Freeman-S <mr.baranok@mail.ru>
2022-03-08 12:30:38 +0300

    refactor 1HM

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -17,6 +17,13 @@ function Message(props: MessageType) {
                 <p>{props.message}</p>
                 <p className={styles.time}>{props.time}</p>
             </div>
+            <svg height="0" width="0">
+                <defs>
+                    <clipPath id="svgPath">
+                        <path fill="#FFFFFF" d="M10,10H0V0H0A10,10,0,0,0,10,10Z"/>
+                    </clipPath>
+                </defs>
+            </svg>
 
         </div>
     )
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -3,7 +3,6 @@ import s from './Error.module.css'
 
 function Error404() {
     return (
-
         <div className={s.main}>
             <p>Page not found</p>
         </div>
