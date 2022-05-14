c7294eed4e684fcb5445c4ac025b9e432600258f
mr-Freeman-S <mr.baranok@mail.ru>
2022-05-14 21:42:44 +0300

    13 homework is done

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -5,14 +5,15 @@ import {RequestAPI} from "./RequestAPI";
 
 const Request = () => {
     const [checkboxValue, setCheckboxValue] = useState<boolean>(false)
-    const [answer,setAnswer]= useState<string|null>(null)
+    const [answer, setAnswer] = useState<string | null>(null)
     const onClickCallback = () => {
         RequestAPI.postCheckBoxValue(checkboxValue)
             .then(res => setAnswer(res.statusText))
-            .catch(err=> setAnswer(err.statusText))
+            .catch(err => setAnswer(err.statusText))
     }
     return (
         <div>
+            <h3>HW13</h3>
             <SuperCheckbox onChangeChecked={setCheckboxValue} checked={checkboxValue}/>
             <SuperButton onClick={onClickCallback}>POST</SuperButton>
             <span>{answer}</span>
--- src/p2-homeworks/h13/RequestAPI.ts
+++ src/p2-homeworks/h13/RequestAPI.ts
@@ -1,11 +1,11 @@
-const requestOptions =(value:boolean)=> ({
+const requestOptions = (value: boolean) => ({
     method: 'POST',
-    headers: {'Content-Type':'application/json'},
-    body:JSON.stringify({success:value})
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({success: value})
 })
 
-export const RequestAPI  = {
-    postCheckBoxValue(value:boolean){
-          return fetch("https://neko-cafe-back.herokuapp.com/auth/test",requestOptions(value))
+export const RequestAPI = {
+    postCheckBoxValue(value: boolean) {
+        return fetch("https://neko-cafe-back.herokuapp.com/auth/test", requestOptions(value))
     }
 }
